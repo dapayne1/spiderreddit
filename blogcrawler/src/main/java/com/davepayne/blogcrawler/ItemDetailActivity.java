090793793b560dc7cfdfd834976919c639dd120f
david_payne <david.payne@atomicaxis.com>
2014-05-26 23:27:42 -0400

    Updated ItemDetails to be WebViews, with full offline cacheing and sweet loading animation.

--- blogcrawler/src/main/java/com/davepayne/blogcrawler/ItemDetailActivity.java
+++ blogcrawler/src/main/java/com/davepayne/blogcrawler/ItemDetailActivity.java
@@ -20,10 +20,15 @@ public class ItemDetailActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set content view for layout from XML.
         setContentView(R.layout.activity_item_detail);
 
         // Show the Up button in the action bar.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String actionBarTitleString = getIntent().getStringExtra("ActionBarTitle");
+        updateActionBarTitle(actionBarTitleString);
 
         // savedInstanceState is non-null when there is fragment state
         // saved from previous configurations of this activity
@@ -48,19 +53,21 @@ public class ItemDetailActivity extends ActionBarActivity {
         }
     }
 
+    /**
+     * Sets the actionbar title to the specified string.
+     * @param newTitle String title to update actionbar with.
+     */
+    public void updateActionBarTitle(String newTitle) {
+        setTitle(newTitle);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
             // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
+            // activity, the Up button is shown.
             onBackPressed();
-            //NavUtils.navigateUpTo(this, new Intent(this, ItemListActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
--- blogcrawler/src/main/java/com/davepayne/blogcrawler/ItemDetailFragment.java
+++ blogcrawler/src/main/java/com/davepayne/blogcrawler/ItemDetailFragment.java
@@ -5,9 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.davepayne.blogcrawler.dummy.DummyContent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 /**
  * A fragment representing a single Item detail screen.
@@ -16,16 +17,17 @@ import com.davepayne.blogcrawler.dummy.DummyContent;
  * on handsets.
  */
 public class ItemDetailFragment extends Fragment {
+
+    private final static String SPIDEY_GIF = "file:///android_asset/spidey.gif";
+    private WebView mWebView;
+    private String urlString = null;
+    private boolean isLoaded = false;
+
     /**
      * The fragment argument representing the item ID that this fragment
      * represents.
      */
     public static final String ARG_ITEM_ID = "item_id";
-
-    /**
-     * The dummy content this fragment is presenting.
-     */
-    private DummyContent.DummyItem mItem;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -38,24 +40,73 @@ public class ItemDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Let's check our bundled arguments for what will be the URL.
         if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+            urlString = getArguments().getString(ARG_ITEM_ID);
+        } else {
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
+
+        // Inflate our root view from XML.
         View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
-        if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.content);
-        }
+        // Assign our web view reference.
+        mWebView = (WebView) rootView.findViewById(R.id.item_detail);
+
+        // Enable Javascript and caching..
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setSupportMultipleWindows(false);
+        mWebView.getSettings().setNeedInitialFocus(false);
+        mWebView.getSettings().setSupportZoom(false);
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        mWebView.getSettings().setAppCachePath(getActivity().getCacheDir().getAbsolutePath());
+        mWebView.getSettings().setAllowFileAccess(true);
+        mWebView.getSettings().setAppCacheEnabled(true);
+
+        // Set up our webview web client and settings.
+        mWebView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getActivity(), description, Toast.LENGTH_LONG).show();
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                // If we are in a !isLoaded state, but we've obviously just finished loading
+                // something, this means our animated gif load animation has just finished.
+                // Proceed to loading urlString.
+                if (!isLoaded) {
+                    loadWebView();
+                }
+            }
+        });
+
+        // Load our webslinging animated hero.
+        mWebView.loadUrl(SPIDEY_GIF);
 
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    /**
+     * Assumes urlString field is non-null, otherwise does nothing. Attempts to load urlString
+     * into web view.
+     */
+    private void loadWebView() {
+        if (urlString != null) {
+            // Load our webview.
+            mWebView.loadUrl(urlString);
+
+            // Update our control logic.
+            isLoaded = true;
+        }
     }
 }
